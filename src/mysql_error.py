28a0c480d01bc525e58d328d90d67aac59b213dc
Josh Tran <josh.tran@lacity.org>
2022-08-18 16:33:00 -0700

    initial commit

---
+++ src/mysql_error.py
@@ -0,0 +1,24 @@
+import apache_beam as beam
+from apache_beam.pipeline import PipelineOptions
+
+def run():
+    pipeline_options = PipelineOptions()
+
+    with beam.Pipeline(options=pipeline_options) as p:
+        initialize = (
+            p
+            | "Initialize Pipeline" >> beam.Create([1,2,3,4,5])
+        )
+
+        _ = (
+            initialize
+            | "Get Table Schema" >> beam.Map(print)
+        )
+
+
+if __name__ == '__main__':
+    run()
+    
+
+
+
